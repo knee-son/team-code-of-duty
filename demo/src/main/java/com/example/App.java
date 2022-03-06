72ca0527eb321f918486f8e88e3f2462bbfa8cfd
knee-son <83338313+knee-son@users.noreply.github.com>
2022-03-06 21:21:43 +0800

    update sa etc

--- demo/src/main/java/com/example/App.java
+++ demo/src/main/java/com/example/App.java
@@ -2,9 +2,12 @@ package com.example;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -21,35 +24,38 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
+
         // scene = new Scene(loadFXML("primary"), 640, 480);
         // stage.setScene(scene);
         // stage.show();
-    
+
         //Creating an image 
         Image image = new Image(new FileInputStream("demo/src/main/resources/chart.png"));  
-
         //Setting the image view 
         ImageView imageView = new ImageView(image);
-
         //setting the fit height and width of the image view 
         imageView.setFitHeight(500); 
-        imageView.setFitWidth(500); 
-
+        imageView.setFitWidth(500);
         //Setting the preserve ratio of the image view 
-        imageView.setPreserveRatio(false);  
+        imageView.setPreserveRatio(false);
+
+        //Creating a Button
+        Button button = new Button();
+        //Setting text to the button
+        button.setText("play sound");
+        //Setting the location of the button
+        button.setTranslateX(212);
+        button.setTranslateY(460);
 
         //Creating a Group object  
-        Group root = new Group(imageView);  
-
-        //Creating a scene object 
-        Scene scene = new Scene(root, 500, 500);  
-
-        //Setting title to the Stage 
-        stage.setTitle("Code of Duteeeeeeehhh");  
-
+        Group root = new Group(imageView,button);
+        //Creating a scene object
+        Scene scene = new Scene(root);  
+        //Setting title to the Stage
+        stage.setTitle("Code of Dutyyy ✊😭");
         //Adding scene to the stage 
         stage.setScene(scene);
-
+        stage.setMaximized(true);
         //Displaying the contents of the stage 
         stage.show(); 
     }
