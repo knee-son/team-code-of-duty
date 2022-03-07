c62eac8f9f63aa54a04de253c634e15f18bf330d
knee-son <83338313+knee-son@users.noreply.github.com>
2022-03-08 05:37:55 +0800

    changed resolution to 850x600; edited body.fxml in scenebuilder; wrapped contents into containers for convenience

--- Circle of Fifths/src/main/java/com/fifths/App.java
+++ Circle of Fifths/src/main/java/com/fifths/App.java
@@ -13,42 +13,11 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-
         scene = new Scene(loadFXML("cover"));
         stage.setScene(scene);
-        stage.setMaximized(true);
+        stage.setResizable(false);
+        stage.setTitle("Chord Guide Chart");
         stage.show();
-
-        // //-- sample without using .fxml--
-        // //Creating an image 
-        // Image image = new Image(new FileInputStream("demo/src/main/resources/chart.png"));  
-        // //Setting the image view 
-        // ImageView imageView = new ImageView(image);
-        // //setting the fit height and width of the image view 
-        // imageView.setFitHeight(500); 
-        // imageView.setFitWidth(500);
-        // //Setting the preserve ratio of the image view 
-        // imageView.setPreserveRatio(false);
-
-        // //Creating a Button
-        // Button button = new Button();
-        // //Setting text to the button
-        // button.setText("play sound");
-        // //Setting the location of the button
-        // button.setTranslateX(212);
-        // button.setTranslateY(460);
-
-        // //Creating a Group object  
-        // Group root = new Group(imageView,button);
-        // //Creating a scene object
-        // Scene scene = new Scene(root);  
-        // //Setting title to the Stage
-        // stage.setTitle("Code of Dutyyy ✊😭");
-        // //Adding scene to the stage 
-        // stage.setScene(scene);
-        // stage.setMaximized(true);
-        // //Displaying the contents of the stage 
-        // stage.show(); 
     }
 
     static void setRoot(String fxml) throws IOException {
--- Circle of Fifths/src/main/java/com/fifths/cover_events.java
+++ Circle of Fifths/src/main/java/com/fifths/cover_events.java
@@ -1,12 +1,21 @@
 package com.fifths;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class cover_events {
+public class cover_events implements Initializable{
 
     @FXML
     private void button_pressed() throws IOException {
         App.setRoot("body_modified");
+    }
+
+    @Override
+    public void initialize(URL u, ResourceBundle r) {
+        // TODO Auto-generated method stub
+        
     }
 }
