a9fec6b7a82dd28fc5396d14395becc4f893a7a2
knee-son <83338313+knee-son@users.noreply.github.com>
2022-03-09 02:10:42 +0800

    Revert "changed resolution to 850x600; edited body.fxml in scenebuilder; wrapped contents into containers for convenience"
    This reverts commit c62eac8f9f63aa54a04de253c634e15f18bf330d.

--- Circle of Fifths/src/main/java/com/fifths/cover_events.java
+++ Circle of Fifths/src/main/java/com/fifths/cover_events.java
@@ -1,21 +1,12 @@
 package com.fifths;
 
 import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 
-public class cover_events implements Initializable{
+public class cover_events {
 
     @FXML
     private void button_pressed() throws IOException {
         App.setRoot("body_modified");
-    }
-
-    @Override
-    public void initialize(URL u, ResourceBundle r) {
-        // TODO Auto-generated method stub
-        
     }
 }
