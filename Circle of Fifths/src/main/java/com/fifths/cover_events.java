f2cc79a75a44291ebd563ae8f2f159e46f751bfb
Reynaldo Subong Jr <subong.reynaldojr@gmail.com>
2022-03-18 17:55:08 +0800

    box

--- Circle of Fifths/src/main/java/com/fifths/cover_events.java
+++ Circle of Fifths/src/main/java/com/fifths/cover_events.java
@@ -8,12 +8,12 @@ import javafx.animation.FadeTransition;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.shape.Box;
+import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 
 public class cover_events implements Initializable{
 
-    @FXML private Box mybox;
+    @FXML private VBox mybox;
     
     @FXML
     private void button_pressed() throws IOException {
