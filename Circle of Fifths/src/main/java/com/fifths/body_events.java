6022e17254bb588e77378cf3338ca1ec9725d141
knee-son <83338313+knee-son@users.noreply.github.com>
2022-03-18 13:34:06 +0800

    added play note functions
    naa sa body_events ang functions. stud pa siya though

--- Circle of Fifths/src/main/java/com/fifths/body_events.java
+++ Circle of Fifths/src/main/java/com/fifths/body_events.java
@@ -7,4 +7,29 @@ import javafx.scene.shape.Circle;
 public class body_events {
     @FXML private Circle circle_outline;
     @FXML private Pane fingering_guide;
+
+    @FXML
+    private void play_Ab(){}
+    @FXML
+    private void play_A(){}
+    @FXML
+    private void play_Bb(){}
+    @FXML
+    private void play_B(){}
+    @FXML
+    private void play_C(){}
+    @FXML
+    private void play_Db(){}
+    @FXML
+    private void play_D(){}
+    @FXML
+    private void play_Eb(){}
+    @FXML
+    private void play_E(){}
+    @FXML
+    private void play_F(){}
+    @FXML
+    private void play_Gb(){}
+    @FXML
+    private void play_G(){}
 }
