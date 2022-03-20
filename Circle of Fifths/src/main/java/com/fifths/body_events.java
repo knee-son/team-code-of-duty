3cab786730698817adb47cecac5c571faa431ac4
knee-son <83338313+knee-son@users.noreply.github.com>
2022-03-20 14:46:19 +0800

    arcs now inside 1 container

--- Circle of Fifths/src/main/java/com/fifths/body_events.java
+++ Circle of Fifths/src/main/java/com/fifths/body_events.java
@@ -2,7 +2,9 @@ package com.fifths;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.shape.Arc;
 import javafx.scene.shape.Circle;
 import java.io.File;
 import java.net.URL;
@@ -12,8 +14,7 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 public class body_events implements Initializable{
-    @FXML private Circle circle_outline;
-    @FXML private Pane fingering_guide;
+    @FXML private GridPane arcs_container;
 
     private static Media[] note_array = new Media[98];
     private MediaPlayer mp_root;
@@ -30,26 +31,12 @@ public class body_events implements Initializable{
     public void initialize(URL u, ResourceBundle r)
     {
         for(int i=30; i<=97; i++){
-            note_array[i] = new Media(
-                new File(
-                    "Circle of Fifths/src/main/"+
-                    "resources/piano_notes/pno0"+
-                    i+".mp3").toURI().toString());
-        }
+            note_array[i] = new Media( new File(
+                "Circle of Fifths/src/main/"+
+                "resources/piano_notes/pno0"+
+                i+".mp3").toURI().toString()); }
+        
     }
-    
-    @FXML private void play_C()  { play(0);  }
-    @FXML private void play_Db() { play(1);  }
-    @FXML private void play_D()  { play(2);  }
-    @FXML private void play_Eb() { play(3);  }
-    @FXML private void play_E()  { play(4);  }
-    @FXML private void play_F()  { play(5);  }
-    @FXML private void play_Gb() { play(6);  }
-    @FXML private void play_G()  { play(7);  }
-    @FXML private void play_Ab() { play(8);  }
-    @FXML private void play_A()  { play(9);  }
-    @FXML private void play_Bb() { play(10); }
-    @FXML private void play_B()  { play(11); }
 
     private void play(int key_without_octave){
         int key = key_without_octave+octave;
@@ -59,6 +46,6 @@ public class body_events implements Initializable{
         mp_root.play(); mp_third.play(); mp_fifth.play();
     }
 
-    // private void octave_up(){octave+=12;}
-    // private void octave_down(){if(octave!=0) octave-=12;}
+    private void octave_up(){octave+=12;}
+    private void octave_down(){if(octave!=0) octave-=12;}
 }
